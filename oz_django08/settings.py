a5f41fb5e73fd3d45e49b6256ecef16ed7ebfbdc
seonhongmun <mshg_@naver.com>
2025-02-26 14:26:32 +0900

    run black

--- oz_django08/settings.py
+++ oz_django08/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
 from typing import reveal_type
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
