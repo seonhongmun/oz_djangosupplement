589988f382838eda92f23e574b16ba07c6d6b6ff
seonhongmun <mshg_@naver.com>
2025-02-27 11:42:38 +0900

    add base model

--- tabom/models.py
+++ tabom/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField("수정일", auto_now=True)
+    created_at = models.DateTimeField("생성일", auto_now_add=True)
 
-class User(models.Model):
+    class Meta:
+        abstract = True
+
+class User(BaseModel):
     name = models.CharField("이름", max_length=50)
-    updated_at = models.DateTimeField("수정일", auto_now=True)
-    created_at = models.DateTimeField("생성일", auto_now_add=True)
 
 
-class Article(models.Model):
+class Article(BaseModel):
     title = models.CharField("제목", max_length=255)
-    updated_at = models.DateTimeField("수정일", auto_now=True)
-    created_at = models.DateTimeField("생성일", auto_now_add=True)
 
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField("수정일", auto_now=True)
-    created_at = models.DateTimeField("생성일", auto_now_add=True)
