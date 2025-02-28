523d131a0deed5279b2b0c4267c1f6fba7e10ef0
seonhongmun <mshg_@naver.com>
2025-02-28 11:51:06 +0900

    test coverage

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,5 +1,18 @@
+from django.core.exceptions import BadRequest
+from django.db import IntegrityError
+
 from tabom.models import Like
 
 
 def do_like(user_id: int, article_id: int) -> Like:
-    return Like.objects.create(user_id=user_id, article_id=article_id)
+    try:
+        return Like.objects.create(user_id=user_id, article_id=article_id)
+    except IntegrityError as e:
+        if "FOREIGN KEY (`user_id`)" in e.args[1]:
+            raise BadRequest(f"없는 user_id 입니다: {user_id}")
+        if "FOREIGN KEY (`article_id`)" in e.args[1]:
+            raise BadRequest(f"없는 article_id 입니다: {article_id}")
+        # if "중복에 의한 integrity error 검사" in e.args[1]:
+        #     raise BadRequest("이미 좋아요 하셨습니다.")
+        raise
+        # raise BadRequest("절대 있어서는 안되는 일이 일어났습니다.")
--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import BadRequest
 from django.db import IntegrityError
 from django.test import TestCase
 
@@ -7,17 +8,17 @@ from tabom.services.like_service import do_like
 
 class TestLikeService(TestCase):
     def test_a_user_can_like_an_article(self) -> None:
-        # Given : 테스트에 필요한 재료를 준비한다.
+        # Given: 테스트에 필요한 재료를 준비한다.
         user = User.objects.create(name="test")
-        article = Article.objects.create(title="test_article")
+        article = Article.objects.create(title="test_title")
 
-        # When : 실제 테스트 대상이 되는 동작을 실행한다.
+        # When: 실제 테스트 대상이 되는 동작을 실행합니다.
         like = do_like(user.id, article.id)
 
-        # Then : 동작을 마친후에 결과가 "예상한 대로" 나왔는지 검증한다.
+        # Then: 동작을 마친후에 결과가 "에상한 대로" 나왔는지 검증합니다.
         self.assertIsNotNone(like.id)
-        self.assertEqual(like.user_id, user.id)
-        self.assertEqual(like.article_id, article.id)
+        self.assertEqual(user.id, like.user_id)
+        self.assertEqual(article.id, like.article_id)
 
     def test_a_user_can_like_an_article_only_once(self) -> None:
         # Given
@@ -25,6 +26,32 @@ class TestLikeService(TestCase):
         article = Article.objects.create(title="test_title")
 
         # Expect
-        do_like(user.id, article.id)
+        like1 = do_like(user.id, article.id)
         with self.assertRaises(IntegrityError):
-            do_like(user.id, article.id)
+            like2 = do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(
+        self,
+    ) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(BadRequest) as e:
+            do_like(invalid_user_id, article.id)
+
+        self.assertEqual(str(e.exception), "없는 user_id 입니다: 9988")
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(
+        self,
+    ) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(BadRequest) as e:
+            do_like(user.id, invalid_article_id)
+
+        self.assertEqual(str(e.exception), "없는 article_id 입니다: 9988")
